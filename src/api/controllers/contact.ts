f145bfffe173a42440c209983f1559470a02b8a2
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-04-12 23:56:55 +0530

    :hammer: refactored create contact endpoint

--- src/api/controllers/contact.ts
+++ src/api/controllers/contact.ts
@@ -1,5 +1,6 @@
 import models from "@models/index";
 import { asyncHandler } from "@utils/errors";
+import logger from "@utils/logger";
 import { formatResponse } from "@utils/response";
 import { SubmitContactStruct } from "@utils/validation";
 import { Router } from "express";
@@ -10,14 +11,22 @@ const contactRouter = Router();
 contactRouter.post(
   "/submit",
   asyncHandler(async (req, res, next) => {
+    logger.info("Inside contact submit handler");
     const [err, data] = validate(req.body, SubmitContactStruct);
 
     if (err) {
+      logger.error(`Invalid request body sent ${err}`);
       res.status(400);
       return next(createError(400, err));
     }
 
-    await models.Contact.create(data);
+    try {
+      await models.Contact.create(data);
+    } catch (ex) {
+      logger.error(
+        `Error occured while submitting contact ${JSON.stringify(ex)}`
+      );
+    }
     return formatResponse({ res, result: { success: true } });
   })
 );
